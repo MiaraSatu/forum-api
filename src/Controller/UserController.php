8d1251f860fe4557155122662bef11cf6c00521c
miarasatu <miarasatu@gmail.com>
2022-07-26 16:57:53 +0300

    change method to post and use serializer in UserController::create

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,13 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
 
 class UserController extends AbstractController
 {
-    public function index(): JsonResponse
+    public function create(Request $request, SerializerInterface $serializer): JsonResponse
     {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+        dd($user);
         return new JsonResponse([
             'message' => 'Welcome to api-test',
             'info' => 'now is '.date('H:i m-d')
