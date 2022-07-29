79a7ad940b5aaa139a78e92e710475d1b05d2136
miarasatu <miarasatu@gmail.com>
2022-07-29 20:15:47 +0300

    create api_comments_create and add create method in CommentController

--- src/Controller/CommentsController.php
+++ src/Controller/CommentsController.php
@@ -3,17 +3,44 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Serializer;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Service\SerializerService;
+use App\Repository\PostRepository;
+use App\Repository\UserRepository;
+use App\Entity\Comment;
 
 class CommentsController extends AbstractController
 {
-    #[Route('/comments', name: 'app_comments')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CommentsController.php',
-        ]);
+    private Serializer $serializer;
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em) {
+        $this->serializer = SerializerService::getSerializer();
+        $this->em = $em;
+    }
+
+    public function create(int $postID, int $userID, Request $request, PostRepository $postRepo, UserRepository $userRepo): JsonResponse {
+        if($post = $postRepo->find($postID)) {
+            if($user = $userRepo->find($userID)) {
+                $comment = $this->serializer->deserialize($request->getContent(), Comment::class, 'json');
+                $comment->setCommentedBy($user);
+                $post->addComment($comment);
+                $this->em->persist($comment);
+                // dd($comment);
+                $this->em->flush();
+                $jsonComment = $this->serializer->serialize($comment, 'json', [AbstractNormalizer::IGNORED_ATTRIBUTES => ['comments', 'postedBy']]);
+
+                return new JsonResponse($jsonComment, Response::HTTP_CREATED, [], true);
+            }
+
+            return new JsonResponse(['message' => 'User non trouvé'], Response::HTTP_NOT_FOUND);
+        }
+
+        return new JsonResponse(['message' => 'Post non trouvé'], Response::HTTP_NOT_FOUND);
     }
 }
