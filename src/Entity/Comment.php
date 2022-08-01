37f5e9c8459b4e908a5f182982eea2a2db1df9b6
miarasatu <miarasatu@gmail.com>
2022-08-01 14:54:57 +0300

    regenerate Comment Entity

---
+++ src/Entity/Comment.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\TimesTampableTrait;
+use App\Repository\CommentRepository;
+use App\Entity\Traits\MergableTrait;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    use TimesTampableTrait;
+    use MergableTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column()]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\ManyToOne(targetEntity: self::class)]
+    private ?self $parent = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getParent(): ?self
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?self $parent): self
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+}
