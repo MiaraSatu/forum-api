6a18818f1ec6930cd801b9e67a77cab20eec29af
miarasatu <miarasatu@gmail.com>
2022-08-01 14:50:07 +0300

    regenerate Post Entity

---
+++ src/Entity/Post.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\TimesTampableTrait;
+use App\Entity\Traits\MergableTrait;
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\DBAL\Types\Types;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    use TimesTampableTrait;
+    use MergableTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column()]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $subject = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $postedBy = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(?string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPostedBy(): ?User
+    {
+        return $this->postedBy;
+    }
+
+    public function setPostedBy(?User $postedBy): self
+    {
+        $this->postedBy = $postedBy;
+
+        return $this;
+    }
+}
