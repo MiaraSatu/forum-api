78c9b74a81c2c88c2421070c184fab40d958737c
miarasatu <miarasatu@gmail.com>
2022-08-01 16:55:31 +0300

    add onDelete cascade in Comment->$parent

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -24,6 +24,7 @@ class Comment
     private ?string $content = null;
 
     #[ORM\ManyToOne(targetEntity: self::class)]
+    #[ORM\JoinColumn(onDelete: "cascade")]
     private ?self $parent = null;
 
     #[ORM\ManyToOne]
